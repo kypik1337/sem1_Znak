08cc8843dae20d1206f5ea01fb093cda70eecae0
Artem <kypidon71@gmail.com>
2022-09-16 21:48:50 +0300

    vin_001

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// задача в которой будет число умножаться на себя(возводиться в квадрат)
+Console.WriteLine("Write number");
+int number = int.Parse(Console.ReadLine());
+//возведение в квадрат математическим методом
+int sqr = number * number;
+Console.WriteLine($"square number {number} = {sqr}");
+//$ это текст + переменные 
+// Метод с помощью библиотеки
+
+int sqr1 = Convert.ToInt32(Math.Pow(number, 3));
+Console.WriteLine($"square number {number} = {sqr1}");
+
